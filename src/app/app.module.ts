40d45d3042575a0cee6b5df49b9833a76514c3ca
Newton <biinewton382@gmail.com>
2017-11-17 12:36:08 +0300

    Add forms component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { FormComponent } from './form/form.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule
